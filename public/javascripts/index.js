7ba77ff04ca72f637377d53333389ceb0a9e3b3f
Drew Neil <andrew.jr.neil@gmail.com>
2010-12-24 11:14:08 +0200

    Switch TabPanel to a Panel with card layout.
    The onItemDisclosure function now reveals a 'Hello, World!' panel.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,28 +18,21 @@ ListDemo = new Ext.Application({
 
         ListDemo.disclosureList = new Ext.List({
             onItemDisclosure: function(record, btn, index) {
-                Ext.Msg.alert('Tap', 'Disclose more info for ' + record.get('firstName'), Ext.emptyFn);
+                ListDemo.Viewport.setActiveItem('detailpanel');
             },
             store: ListDemo.ListStore,
             itemTpl: '<div class="contact">{firstName} {lastName}</div>'
         });
 
-        ListDemo.List = new Ext.TabPanel ({
+        ListDemo.detailPanel = new Ext.Panel({
+            id: 'detailpanel',
+            html: 'Hello, world!'
+        });
+
+        ListDemo.Viewport = new Ext.Panel ({
             fullscreen: true,
-            items: [{
-                title: 'Simple',
-                items: [ListDemo.simpleList]
-            }, {
-                title: 'Grouped',
-                items: [ListDemo.groupedList]
-            }, {
-                title: 'Disclosure',
-                items: [ListDemo.disclosureList]
-            }],
-            defaults: {
-                layout: 'fit',
-                cls: 'demo-list',
-            }
+            layout: 'card',
+            items: [ListDemo.disclosureList, ListDemo.detailPanel]
         });
     }
 });
