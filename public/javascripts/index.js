71758fa2444e5f22da42da3d595ea20026ed868d
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-17 21:22:29 +0200

    Remove unused list panels.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,19 +3,6 @@ ListDemo = new Ext.Application({
 
     launch: function() {
 
-        ListDemo.simpleList = new Ext.List({
-            xtype: 'list',
-            store: ListDemo.ListStore,
-            itemTpl: '<div class="contact">{firstName} {lastName}</div>'
-        });
-
-        ListDemo.groupedList = new Ext.List({
-            store: ListDemo.ListStore,
-            itemTpl: '<div class="contact">{firstName} {lastName}</div>',
-            grouped: true,
-            indexBar: true
-        });
-
         ListDemo.disclosureList = new Ext.List({
             id: 'disclosurelist',
             onItemDisclosure: function(record, btn, index) {
