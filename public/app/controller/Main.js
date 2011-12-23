9ee2560c270de004fb6d7acece7ce4144a38bfe4
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-23 13:40:29 +0100

    Create a Bondgirls store class.

--- public/app/controller/Main.js
+++ public/app/controller/Main.js
@@ -2,6 +2,7 @@ Ext.define('ListDemo.controller.Main', {
     extend: 'Ext.app.Controller',
     
     models: ['Bondgirl'],
+    stores: ['Bondgirls'],
     views: ['Viewport', 'Home', 'Detail'],
     
     refs: [],
--- public/app/view/Viewport.js
+++ public/app/view/Viewport.js
@@ -8,31 +8,7 @@ Ext.define('ListDemo.view.Viewport', {
         items: [
             {
                 xtype: 'homepanel',
-                store: {
-                    model: 'ListDemo.model.Bondgirl',
-                    data: [
-                        { firstName: "Domino",      lastName: "Derval" },
-                        { firstName: "Elektra",     lastName: "King" },
-                        { firstName: "Fiona",       lastName: "Volpe" },
-                        { firstName: "Holly",       lastName: "Goodhead" },
-                        { firstName: "Honey",       lastName: "Rider" },
-                        { firstName: "Jill",        lastName: "Masterton" },
-                        { firstName: "Kissy",       lastName: "Suzuki" },
-                        { firstName: "Mary",        lastName: "Goodnight" },
-                        { firstName: "Miranda",     lastName: "Frost" },
-                        { firstName: "Molly",       lastName: "Warmflash" },
-                        { firstName: "Paula",       lastName: "Caplan" },
-                        { firstName: "Penelope",    lastName: "Smallbone" },
-                        { firstName: "Plenty",      lastName: "O'Toole" },
-                        { firstName: "Pussy",       lastName: "Galore" },
-                        { firstName: "Strawberry",  lastName: "Fields" },
-                        { firstName: "Sylvia",      lastName: "Trench" },
-                        { firstName: "Tatiana",     lastName: "Romanova" },
-                        { firstName: "Tilly",       lastName: "Masterton" },
-                        { firstName: "Vesper",      lastName: "Lynd" },
-                        { firstName: "Xenia",       lastName: "Onatopp" }
-                    ]
-                }
+                store: 'ListDemo.store.Bondgirls'
             },
             {
                 xtype: 'detailpanel'
